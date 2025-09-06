cd9b6d4d5eb4712a2174f7e03164cce59d650f49
Gowtham Sundrasekaran <gowtham.s@kggroup.in>
2025-09-06 05:57:02 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import MenuForm from './components/MenuForm';
 import CafeteriaForm from './components/CafeteriaForm';
 import OurCampus from './components/OurCampus';
 import Security from './components/Security';
+import EmployeeForm from './components/EmployeeForm';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+ 
 function App() {
   return (
     <>
@@ -19,11 +20,11 @@ function App() {
           <Route path="/cafeteria-form" element={<CafeteriaForm />} />
           <Route path="/our-campus" element={<OurCampus />} />
           <Route path="/security" element={<Security />} />
+          <Route path="/employeeForm" element={<EmployeeForm />} />
         </Routes>
       </Router>
     </>
   )
 }
-
+ 
 export default App;
-
